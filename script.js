368137482d16eed9148a126dfd524e6f4b380079
Yauheni Shatau <jack13only@gmail.com>
2021-08-10 11:42:03 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
-const btnUp = document.querySelector('btn-up')
-const btnDown = document.querySelector('btn-Down')
+const next = document.querySelector('.btn-up')
+const prev = document.querySelector('.btn-down')
 const slides = document.querySelectorAll('.slide')
-console.log(slides)
+const dots = document.querySelectorAll('.dots')
+
+
+let index = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('active')
+    }
+    slides[n].classList.add('active')
+}
+
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('active')
+    }
+    dots[n].classList.add('active')
+}
+
+const changeSlide = (x) => {
+    activeSlide(x)
+    activeDot(x)
+}
+
+const nextSlide = () => {
+    if (index === slides.length - 1) {
+        index = 0;
+        changeSlide(index)
+    } else {
+        index++;
+        changeSlide(index)
+    }
+}
+
+const prevSlide = () => {
+    if (index === 0) {
+        index = slides.length - 1;
+        activeSlide(index)
+    } else {
+        index--;
+        activeSlide(index)
+    }
+}
+
+next.addEventListener('click', () => {
+    nextSlide()
+})
+
+prev.addEventListener('click', () => {
+    prevSlide()
+})
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', () => {
+        changeSlide(i)
+    })
+}
